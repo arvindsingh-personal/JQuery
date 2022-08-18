3c1e5e02a3877c65e34e618e3c186f8e0d4d84ed
arvindsingh-cedcommerce <arvindsingh@cedcoss.com>
2022-08-18 19:05:23 +0530

    Food Delivery

---
+++ data.js
@@ -0,0 +1,44 @@
+const data = [
+    {
+        "id":1,
+        "menu":"Veg",
+        "name":"Burger",
+        "price":40.00,
+        "image":"images/burger.jpg",
+    },
+    {
+        "id":2,
+        "menu":"Veg",
+        "name":"Plane Dosa",
+        "price":30.00,
+        "image":"images/dosa.jpg",
+    },
+    {
+        "id":3,
+        "menu":"Non Veg",
+        "name":"Tanduri Chicken",
+        "price":220.00,
+        "image":"images/tanduri_chicken.webp",
+    },
+    {
+        "id":4,
+        "menu":"Veg",
+        "name":"Paneer",
+        "price":180.00,
+        "image":"images/paneer.jpg",
+    },
+    {
+        "id":5,
+        "menu":"Veg",
+        "name":"Mix Salad",
+        "price":80.00,
+        "image":"images/salad.jpg",
+    },
+    {
+        "id":6,
+        "menu":"Veg",
+        "name":"Mix Vegies",
+        "price":100.00,
+        "image":"images/mix-veg.jpg",
+    }
+]
---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import ResponsiveAppBar from './Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <ResponsiveAppBar />
+    </div>
+  );
+}
+
+export default App;
